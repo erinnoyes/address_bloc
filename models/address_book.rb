e25f0b6dba3047fa3d08363bfebc56854e7716a7
Erin Noyes <emnoyes@gmail.com>
2018-03-14 07:32:21 -0700

    Assignment 4 Models: Update 2

--- models/address_book.rb
+++ models/address_book.rb
@@ -30,35 +30,5 @@ def add_entry(name, phone_number, email)
     end
     entries.insert(index, Entry.new(name, phone_number, email))
 end
+end 
 
-<<<<<<< Updated upstream
-def remove_entry(name, phone_number, email)
-    entries.each do |entry|
-        if name == entry.name &&  phone_number == entry.phone_number && email == entry.email
-            entries.remove(entry)
-        end
-    end
-end
-||||||| merged common ancestors
-  def import_from_csv(file_name)
-      csv_text = File.read(file_name)
-      csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
-      
-      csv.each do |row|
-          row_hash = row.to_hash
-          add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
-      end 
-  end
-=======
-
-  def import_from_csv(file_name)
-      csv_text = File.read(file_name)
-      csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
-      
-      csv.each do |row|
-          row_hash = row.to_hash
-          add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
-      end 
-  end
->>>>>>> Stashed changes
-end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -53,24 +53,6 @@ RSpec.describe AddressBook do
             expect(new_entry.email).to eq('augusta.king@lovelace.com')
         end
     end
-    
-    describe "#remove_entry" do
-        it "removes only one entry to the address book" do
-            book = AddressBook.new
-            book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-            
-            expect(book.entries.size).to eq(0)
-        end
-<<<<<<< Updated upstream
-    end 
- end
-||||||| merged common ancestors
-    end
-end
-=======
-    end
+  end
 
-    describe "importing entries from "
-end
->>>>>>> Stashed changes
 
